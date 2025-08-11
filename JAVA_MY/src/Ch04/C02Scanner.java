3bcbe21a1225e600111640de655b7b38044327a2
Doidoria <th15926@daum.net>
2025-08-11 17:17:18 +0900

    day8-11(2)

--- JAVA_MY/src/Ch04/C02Scanner.java
+++ JAVA_MY/src/Ch04/C02Scanner.java
@@ -11,14 +11,24 @@ public class C02Scanner {
 		System.out.print("정수 입력 :");
 		int num1=sc.nextInt();
 		System.out.println("입력된 정수 값 :"+num1);
-		
+
 		System.out.print("실수 입력 :");
 		double num2=sc.nextDouble();
 		System.out.println("입력된 실수 값 :"+num2);
-		
+	
 		System.out.println("문자열 입력 :");
 		String str=sc.next();
 		System.out.println("입력된 문자열 : "+str);
+		
+		System.out.println("입력 : ");
+		String s1=sc.next();
+		String s2=sc.next();
+		String s3=sc.next();
+		System.out.printf("문자열 : %s %s %s\n",s1,s2,s3);
+		
+		System.out.println("문자열 입력(띄어쓰기 포함) :");
+		String str2=sc.nextLine();
+		System.out.println("문자열 : "+str2);
 	}
 
 }
